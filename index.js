fa7e3459fae708ca09d0ec2ee7c72be34443084c
Ruma <matteoh8@hotmail.it>
2023-07-01 19:21:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//document.writeln("Ciao da js/index.js");
+
+
+// Commento single line
+
+/*
+Commento
+multilinea
+*/
+
+//alert('Questo è un messaggio di avviso.');
+//const name = prompt("Come va?");
+/*
+const name = prompt('Come ti chiami?', 'Inserisci qui il tuo nome');
+console.log(name);
+const result = confirm('Sei sicuro che sia questo il tuo vero nome?');
+console.log(result);
+if (result == true) {
+    console.log("Ciao " + name);
+}
+else {
+    console.log("Ritenta l'autenticazione");
+}
+*/
+//* Metodi Console
+/*
+console.log('Sono una stringa');
+console.log("10");
+console.log(typeof console);
+console.log(typeof console.log);
+*/
+
+//console.clear();
+
+//console.warn("Messaggio di avviso");
+
+//console.error("Messaggio di errore");
